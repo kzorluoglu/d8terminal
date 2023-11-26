48cefdfc1a7e7001cecf885629e58af0189d0397
Koray Zorluoglu <koray@d8devs.com>
2023-11-26 23:12:48 +0100

    missing tags added in style.css

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
     <div id="terminal">
 	    <div id="history"></div>
     </div>
---
+++ unused.php
@@ -0,0 +1,2 @@
+<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<?php wp_link_pages(); ?>
